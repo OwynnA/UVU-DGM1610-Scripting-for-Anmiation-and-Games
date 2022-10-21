beb0775b7fe3dcee6c8dca40f17cfb5ef5927b09
Owynn Armstrong <10932343@uvu.edu>
2022-10-21 00:31:51 -0600

    Added the rest of working pumpkin jump and removed the broken version

--- 2D platformer/Assets/Scripts/CoinSpawnManager.cs
+++ 2D platformer/Assets/Scripts/CoinSpawnManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CoinSpawnManager : MonoBehaviour
 {
     public GameObject[] coinPrefabs;
+    [SerializeField]
     public float spawnRangeX;
+    [SerializeField]
     public float spawnPosY;
     public int spawnRateBig;
     public int bigSpawn = 0;
