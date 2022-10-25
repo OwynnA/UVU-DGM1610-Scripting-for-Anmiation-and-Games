8b84ce307f3c508e862f07ccb426ed20a082c415
Owynn Armstrong <10932343@uvu.edu>
2022-10-25 12:56:41 -0600

    saved changed to pumpkin game

--- 2D platformer/Assets/Scripts/EnemyPatrol.cs
+++ 2D platformer/Assets/Scripts/EnemyPatrol.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float rayDistance;
+    private bool isMovingRight = true;
+    public transform groundDetection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.RayCast(groundDetection.position, vector2.down, rayDistance);
+        if(groundInfo.collider == false)
+        {
+            if(isMovingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                isMovingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles  new Vector3(0, 0, 0);
+                isMovingRight = true;
+            }
+        }
     }
 }
