a006769adba6d687f2ebcaae83a0c81fc69556ac
Owynn Armstrong <10932343@uvu.edu>
2022-11-07 21:24:09 -0700

    saved changes before closing

--- 2D platformer/Assets/Scripts/Enemy.cs
+++ 2D platformer/Assets/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int health;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Enemy done died.");
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Take that! And that! And a little bit of this!");
     }
 }
--- 2D platformer/Assets/Scripts/PlayerAttack.cs
+++ 2D platformer/Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private float attackDelay;
+    private float startDelay;
+    public transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,26 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(attack dealy <= 0) //if attack delay is zero, fire at will
+        {
+            if(input.GetKey(KeyCode.Space)) //wait for the signal
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            attackDelay = startDelay;
+        }
+        else
+        {
+            atackDelay -= Time.deltaTime;//its the final countdown!!
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.Position, attackRange)
     }
 }
