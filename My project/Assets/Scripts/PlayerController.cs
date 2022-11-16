4d8b00604eed074c2b9108cce59e5027f30bd739
Owynn Armstrong <10932343@uvu.edu>
2022-11-16 03:27:38 -0700

    Added player Controller script and fixed camera issues

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Movement")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Camera")]
+    public float lookSensitivity;
+    public float maxLookX;
+    public float minLookX;
+    public float rotX;
+    private Camera camera;
+    private Rigidbody rb;
+    void Awake()
+    {
+        //Get Components
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        CameraLook();
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+        rb.velocity = new Vector3(x, rb.velocity.y, z);
+
+    }
+    void CameraLook()
+    {
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
     }
 }
