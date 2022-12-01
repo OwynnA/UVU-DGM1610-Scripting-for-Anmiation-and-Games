904236eea4225b67b29b4b0bf804ab32c114adb4
Owynn Armstrong <10932343@uvu.edu>
2022-11-30 17:14:22 -0700

    Fixed pickup script and worked game a bit more

--- My project/Assets/Scripts/Pickup.cs
+++ My project/Assets/Scripts/Pickup.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public PickupType type;
+    public int value;
+    public int ammoAmount;
+    [Header("Bobbing Motion")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+    private bool bobbingUp;
+    private Vector3 startPos;
+
+    public AudioClip pickupSFX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+    }
+    public enum PickupType
+    {
+        Health,
+        Ammo
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+
+            switch(type)
+            {
+                case PickupType.Health:
+                player.GiveHeatlh(value);
+                break;
+                case PickupType.Ammo:
+                player.GiveAmmo(ammoAmount);
+                break;
+
+                default:
+                print("Type not accepted");
+                break;
+            }
+            other.GetComponent<AudioSource>.PlayOneShot(pickupSFX);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        //rotates pickup around y axis
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        Vector3 offset = (bobbingUp == true ? new Vector3(0, bobHeight /2, 0) : new Vector3(0, -bobHeight /2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+        if(transform.position == startPos + offset)
+        {
+            bobbingUp = !bobbingUp;
+        }
     }
 }
